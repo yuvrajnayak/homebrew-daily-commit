e8735968e4ae26930fe7826e018dbd74bc72a2e1
Yuvraj Nayak <yuvrajnayak@hotmail.com>
2020-03-16 13:47:56 -0400

    updated sha256

--- Formula/dcom.rb
+++ Formula/dcom.rb
@@ -2,7 +2,7 @@ class Dcom < Formula
     desc "Bash script to commit file changes daily"
     homepage "github.com/yuvrajnayak/homebrew-daily-commit"
     url "https://github.com/yuvrajnayak/homebrew-daily-commit/archive/v1.2.0.tar.gz"
-    sha256 "50470059c23d8c3c40146fef2d9d97fc9debef726a828642624a90870ac57bfd"
+    sha256 "18e20110a062d889eebfb58a5f44898e223dad855a32ff7c73facaa796499003"
 
     def install
         bin.install "homebrew-daily-commit/dcom"
