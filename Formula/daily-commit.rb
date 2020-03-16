a3e9fe6525455c279d5e27a38e472e81d9a9a7b4
Yuvraj Nayak <yuvrajnayak@hotmail.com>
2020-03-16 12:40:57 -0400

    added homebrew formula

---
+++ Formula/daily-commit.rb
@@ -0,0 +1,11 @@
+class DailyCommit < Formula
+    desc "Bash script to commit file changes daily"
+    homepage "github.com/yuvrajnayak/daily-commit"
+    url "https://github.com/yuvrajnayak/daily-commit/archive/1.0.0.tar.gz"
+    sha256 "50470059c23d8c3c40146fef2d9d97fc9debef726a828642624a90870ac57bfd"
+
+    def install
+        bin.install dcom
+    end
+
+end
