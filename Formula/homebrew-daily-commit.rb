b37c98e2f7db8453401e61b3687253ce69a2b45c
Yuvraj Nayak <yuvrajnayak@hotmail.com>
2020-03-16 13:05:50 -0400

    added hombrew compatability

--- Formula/homebrew-daily-commit.rb
+++ Formula/homebrew-daily-commit.rb
@@ -1,7 +1,7 @@
 class DailyCommit < Formula
     desc "Bash script to commit file changes daily"
-    homepage "github.com/yuvrajnayak/daily-commit"
-    url "https://github.com/yuvrajnayak/daily-commit/archive/1.0.0.tar.gz"
+    homepage "github.com/yuvrajnayak/homebrew-daily-commit"
+    url "https://github.com/yuvrajnayak/daily-commit/archive/1.1.0.tar.gz"
     sha256 "50470059c23d8c3c40146fef2d9d97fc9debef726a828642624a90870ac57bfd"
 
     def install
