18caf79c83bc5f3f2be02b9f86d4bc1804e21124
Yuvraj Nayak <yuvrajnayak@hotmail.com>
2020-03-17 10:21:52 -0400

    updated tarball

--- Formula/dcom.rb
+++ Formula/dcom.rb
@@ -1,7 +1,7 @@
 class Dcom < Formula
     desc "Bash script to commit file changes daily"
     homepage "github.com/yuvrajnayak/homebrew-daily-commit"
-    url "https://github.com/yuvrajnayak/homebrew-daily-commit/archive/v1.3.0.tar.gz"
+    url "https://github.com/yuvrajnayak/homebrew-daily-commit/archive/v1.4.0.tar.gz"
     sha256 "673d3fbee7f17df0d27db80b69e155a2dc819c7ab08d780552865ddf2074fd30"
 
     def install
